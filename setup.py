b115b7c4c168addfc8e784e72c5ef93ee6c2a79d
Dan <cobbdb@gmail.com>
2014-02-06 07:58:30 -0800

    Fix website in setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     author_email = "opensource@coxinc.com",
     description = "A Django 1.3+ template tag to keep JavaScript out of your templates.",
     license = 'LICENSE.txt',
-    url = "https://github.com/cobbdb/jsload",
+    url = "https://github.com/cobbdb/jsinclude",
     zip_safe = False,
     keywords = "javascript, loader, templatetag, django",
     install_requires = [
